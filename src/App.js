5b4a40c0795bca9be026c5c7a9d0e5bd99b89a54
wobbethan <wobbethan@gmail.com>
2023-05-29 00:20:20 -0400

    pre tmdb

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './HomeScreen';
+import React from "react";
+import "./App.css";
+import HomeScreen from "./HomeScreen";
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen/>
+      <HomeScreen />
     </div>
   );
 }
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import Banner from "./Banner";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
+      <Banner />
     </div>
-    //Nav
     //banner
     //rows
   );
