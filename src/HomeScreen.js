b20e328585e555c1f5aefd76f8e59eace78874d4
wobbethan <wobbethan@gmail.com>
2023-05-29 12:27:01 -0400

    rows done

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -13,15 +13,43 @@ function HomeScreen() {
       <Rows
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
+        isLargeRow={true}
       />
-      <Rows title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Rows title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Rows title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Rows title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Rows title="Horror Movies" fetchUrl={requests.fetchHorroMovies} />
-      <Rows title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Rows title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Rows
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+        isLargeRow={false}
+      />
+      <Rows
+        title="Top Rated"
+        fetchUrl={requests.fetchTopRated}
+        isLargeRow={false}
+      />
+      <Rows
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+        isLargeRow={false}
+      />
+      <Rows
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+        isLargeRow={false}
+      />
+      <Rows
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorroMovies}
+        isLargeRow={false}
+      />
+      <Rows
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+        isLargeRow={false}
+      />
+      <Rows
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+        isLargeRow={false}
+      />
     </div>
     //rows
   );
--- src/Rows.js
+++ src/Rows.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
 
-function Rows(props, isLargeRow = false) {
+function Rows(props) {
   const [movies, setMovies] = useState([]);
   const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -21,9 +21,11 @@ function Rows(props, isLargeRow = false) {
       <div className="row_posters">
         {movies.map((movie) => (
           <img
-            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            className={`row_poster ${
+              props.isLargeRow ? "row_posterLarge" : ""
+            }`}
             src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
+              props.isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
           />
         ))}
