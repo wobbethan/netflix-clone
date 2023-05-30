6d7defbc9b258bdc4b75f44b11defa051a60fe01
wobbethan <wobbethan@gmail.com>
2023-05-30 00:44:53 -0400

    redux implement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens//HomeScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./screens/Login";
+import { auth } from "./firebaseConfig";
+import { useDispatch } from "react-redux";
+import { logout, login } from "./features/userSlice";
 
 function App() {
   const user = null;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //login
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        //logout
+        dispatch(logout);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
   return (
     <div className="app">
       <BrowserRouter>
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -13,13 +13,26 @@ function SignupScreen() {
         emailRef.current.value,
         passwordRef.current.value
       )
-      .then(() => {})
+      .then((authUser) => {
+        console.log(authUser);
+      })
       .catch((error) => {
         alert(error.message);
       });
   };
   const signIn = (e) => {
     e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
